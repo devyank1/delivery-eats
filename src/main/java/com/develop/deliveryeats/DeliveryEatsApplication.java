bb41404bee931280e79d87148347bc39d1e4dcd0
Yan Moreira <yancarlostrab@gmail.com>
2026-02-26 18:03:08 -0300

    first architecture commit

---
+++ src/main/java/com/develop/deliveryeats/DeliveryEatsApplication.java
@@ -0,0 +1,13 @@
+package com.develop.deliveryeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryEatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryEatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/develop/deliveryeats/DeliveryEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop.deliveryeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryEatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
