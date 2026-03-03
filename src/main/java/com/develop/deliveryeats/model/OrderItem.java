88fa3a53a5fc03fdc16ce10c2deb62c0526bfce1
Yan Moreira <contact@yancarlos.tech>
2026-03-03 11:32:35 -0300

    Order Item and Status History created

---
+++ src/main/java/com/develop/deliveryeats/model/OrderItem.java
@@ -0,0 +1,60 @@
+package com.develop.deliveryeats.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_orderitem")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String orderItemId;
+    @OneToOne
+    @JoinColumn(name = "order_id")
+    private Order orderId;
+    @OneToOne
+    @JoinColumn(name = "menuitem_id")
+    private MenuItem menuItemId;
+    private Long quantity;
+    private Double price;
+
+    public String getOrderItemId() {
+        return orderItemId;
+    }
+
+    public void setOrderItemId(String orderItemId) {
+        this.orderItemId = orderItemId;
+    }
+
+    public Order getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Order orderId) {
+        this.orderId = orderId;
+    }
+
+    public MenuItem getMenuItemId() {
+        return menuItemId;
+    }
+
+    public void setMenuItemId(MenuItem menuItemId) {
+        this.menuItemId = menuItemId;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
