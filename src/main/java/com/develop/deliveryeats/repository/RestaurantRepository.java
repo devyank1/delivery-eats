0bf76182c3ce84ad7ee5765e6ef00bda2eb95ca8
Yan Moreira <contact@yancarlos.tech>
2026-03-26 11:10:26 -0300

    Persistency layer added

---
+++ src/main/java/com/develop/deliveryeats/repository/RestaurantRepository.java
@@ -0,0 +1,10 @@
+package com.develop.deliveryeats.repository;
+
+import com.develop.deliveryeats.model.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, UUID> {}
